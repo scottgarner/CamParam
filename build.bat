81ef658182a6369be1928ca8aff1e0d352bc9819
Scott Garner <scott@j38.net>
2021-04-19 14:14:26 -0700

    Updated bat for gc..

--- build.bat
+++ build.bat
@@ -1,1 +1,10 @@
-cl.exe /Os camparam.cpp ole32.lib strmiids.lib oleaut32.lib
+REM Build
+
+set filename=CamParam
+gcc "%filename%".cpp -Os -s -lole32 -loleaut32 -lstrmiids -Wall -DNDEBUG -o "%filename%".exe
+
+REM Tests
+
+REM CamParam.exe
+REM CamParam.exe device 0
+REM CamParam.exe device 0 gain 64
